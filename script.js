21e093d175bfa5b1f97452a54b2e154cbf186ee2
Catherine Li <catherine.uk.20@gmail.com>
2021-07-20 17:51:41 +0100

    implement precedence rules

--- script.js
+++ script.js
@@ -19,10 +19,48 @@ for (let i = 0; i < input.length; i += 2) {
 }
 
 console.log("input", input);
-let result = input[0];
-for (let i = 1; i < input.length - 1; i += 2) {
-  result = METHOD[input[i]](result, input[i + 1]);
-  console.log("Interim", result);
+
+let precedenceCheck = false;
+let result = null;
+
+do {
+  precedence();
+} while (precedenceCheck === false);
+
+calculation();
+console.log("Final result", result);
+
+function precedence() {
+  for (let i = 1; i < input.length - 1; i += 2) {
+    if (
+      input[i] === "times" ||
+      input[i] === "*" ||
+      input[i] === "divided" ||
+      input[i] === "/"
+    ) {
+      input[i - 1] = METHOD[input[i]](input[i - 1], input[i + 1]);
+
+      input.splice(i, 2);
+      console.log(input);
+      break;
+    }
+
+    if (i >= input.length - 2) precedenceCheck = true;
+  }
 }
 
-console.log("Final result", result);
+function calculation() {
+  for (let i = 1; i < input.length - 1; i += 2) {
+    if (result === null) result = METHOD[input[i]](input[i - 1], input[i + 1]);
+    else result = METHOD[input[i]](result, input[i + 1]);
+    console.log("Interim", result);
+  }
+}
+
+// let result = input[0];
+// for (let i = 1; i < input.length - 1; i += 2) {
+//   result = METHOD[input[i]](result, input[i + 1]);
+//   console.log("Interim", result);
+// }
+
+// console.log("Final result", result);
